af4afd8242e6199b0eea88b9b9b8a59ba7012e21
coombalovni <pcoumbassa@gmail.com>
2015-03-09 08:52:18 +0100

    Fondation

--- js/jquery.js
+++ js/jquery.js
@@ -5,17 +5,47 @@
 
 $(document).ready(function(){
 
-  	$('.single-item').slick({
-  	    slide: 'img',
-        dots: true,
-        speed: 500,
-        arrows: true,
-        appendArrows: $('img'),
-        slidesToShow: 2,
-		slidesToScroll: 1,
-		autoplay: true,
-  		autoplaySpeed: 2000,
-  });
+
+    $('.single-item').slick({
+    slide: 'img',
+    dots: true,
+    speed: 500,
+    arrows: true,
+    appendArrows: $('img'),
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    });
+    
+    var animatedDivs = new Array()
+    var i = 0
+    $('.animate-waypoint').each(function() {
+        animatedDivs[i] = $(this);
+        i++;
+    });
+  
+
+    for (i = 0; i < animatedDivs.length; i++) {
+        var div = animatedDivs[i];
+        div.waypoint({
+            handler: function(direction) {
+                if (direction == 'down') {// up, down, left, or right
+                    applyEffect($(this));
+                }
+            },
+            offset: 'bottom-in-view'
+        });
+    }
+
+
+
+
+    function applyEffect(div) {
+        var effect = div.data('animation');
+        div.removeClass("transparent").addClass("opaque").addClass(effect);
+
+    }
 });
 
 
